36ad125983bb020709cc508aa69fda734ee409ee
Brian Falasz <brianfalasz@gmail.com>
2018-12-04 11:52:04 -0600

    finished up authentication! Added cookieSession to index.js and serialize and deserialize to passport.js.

--- server/services/passport.js
+++ server/services/passport.js
@@ -5,6 +5,17 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) =>{
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then ((user) => {
+      done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy({
     clientID: keys.googleClientID,
@@ -12,13 +23,15 @@ passport.use(
     callbackURL: '/auth/google/callback'
   }, (accessToken, refreshToken, profile, done) => {
 
-    User.findOne({ googleId: profile.id })
-      .then((existingUser) => {
-        if(existingUser){
-         
-        } else {
-          new User({ googleId: profile.id }).save();
-        }
-      });
+  User.findOne({ googleId: profile.id })
+    .then((existingUser) => {
+      if(!existingUser){
+        new User({ googleId: profile.id })
+          .save()
+          .then(user => done(null, user));
+      } else {
+        done(null, existingUser);
+      }
+    });
   })
 );
