d92f4a4e6f5b12d23c17393b545a4487ab51046b
Brian Falasz <brianfalasz@gmail.com>
2018-12-03 23:10:55 -0600

    finished up setting up routes for authantication with passport. Set up node mon.

--- server/index.js
+++ server/index.js
@@ -10,8 +10,10 @@ passport.use(
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-  }, (accessToken) => {
-    console.log(accessToken);
+  }, (accessToken, refreshToken, profile, done) => {
+    console.log('access token', accessToken);
+    console.log('refreshToken', refreshToken);
+    console.log('profile', profile);
   })
 );
 
@@ -21,7 +23,7 @@ app.get('/auth/google',
   })
 );
 
-app.get('auth/google/callback', passport.authenticate('google'));
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 
