9bee68c2e7b4fc629539e040ac9548f5caccf4ac
Brian Falasz <brianfalasz@gmail.com>
2018-12-09 01:55:59 -0600

    refactored the passport files. Used async and await to make asynchronous api calls.

--- server/services/passport.js
+++ server/services/passport.js
@@ -22,17 +22,16 @@ passport.use(
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
     proxy: true
-  }, (accessToken, refreshToken, profile, done) => {
+  },
+    async (accessToken, refreshToken, profile, done) => {
 
-  User.findOne({ googleId: profile.id })
-    .then((existingUser) => {
-      if(!existingUser){
-        new User({ googleId: profile.id })
-          .save()
-          .then(user => done(null, user));
-      } else {
-        done(null, existingUser);
-      }
-    });
-  })
+    const existingUser = await User.findOne({ googleId: profile.id })
+      if(existingUser){
+        return done(null, existingUser);
+      } 
+      
+      const user = await new User({ googleId: profile.id }).save()
+      done(null, user);
+    }
+  )
 );
