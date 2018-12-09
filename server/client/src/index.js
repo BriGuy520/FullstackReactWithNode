2996e86bf14dbce4eaeecc92dbb813edca2e8c00
Brian Falasz <brianfalasz@gmail.com>
2018-12-09 14:30:38 -0600

    added reducers files and wired up redux, react-redux, react-router-dom.

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+import App from './components/App';
+
+const store = createStore(() => reducers, {}, applyMiddleware());
+
+ReactDOM.render(
+<Provider store={store}>
+  <App />
+</Provider>, 
+document.querySelector('#root'));
