62e646148e0202a7630bbdc8d4706498767c8894
Brian Falasz <brianfalasz@gmail.com>
2018-12-15 15:35:43 -0600

    added react redux to the survey form review component to get access to the values in the form

--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const SurveyFormReview = () => {
+const SurveyFormReview = ({ onCancel }) => {
   return (
     <div>
       <h5>Please confirm your entries</h5>
+      <button className="yellow darken-3 btn-flat" onClick={onCancel}>Back</button>
     </div>
   );
 }
 
-export default SurveyFormReview;
+const mapStateToProps = (state) => {
+  console.log(state);
+  return {
+    formValues: state.form.suveyValues.values
+  };
+}
+
+export default connect(mapStateToProps)(SurveyFormReview);
