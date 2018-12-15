32ef0b8e2a848399d4bb1ace5b850b4f091c8c60
Brian Falasz <brianfalasz@gmail.com>
2018-12-15 15:18:07 -0600

    fixed the email validation and added a form review component.

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form'; 
 import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
+import validateEmails from '../../utils/validateEmails';
 
 const FIELDS = [
   { label: "Survey Title", name: "title"},
@@ -22,7 +23,7 @@ class SurveyForm extends Component {
   render(){
     return (
       <div>
-        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+        <form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
           {this.renderFields()}
           <Link to="/surveys" className="red btn-flat left white-text">Cancel</Link>
           <button className="teal btn-flat right white-text" type="submit">Next<i className="material-icons right">done</i></button>
@@ -34,6 +35,8 @@ class SurveyForm extends Component {
 
 function validate(values){
   const errors = {};
+
+  errors.emails = validateEmails(values.emails || '');
 
   _.each(FIELDS, ({ name }) => {
     if(!values[name]){
